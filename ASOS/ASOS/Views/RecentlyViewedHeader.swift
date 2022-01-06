3908b273d2b12aac1075908f648dc6cc58274279
Suhayl Ahmed <suhayl.ahmed@icloud.com>
2022-01-06 02:39:57 +0000

    Added Dynamic Type support for Recently Viewed section header

--- ASOS/ASOS/Views/RecentlyViewedHeader.swift
+++ ASOS/ASOS/Views/RecentlyViewedHeader.swift
@@ -13,7 +13,7 @@ final class RecentlyViewedHeader: UICollectionReusableView {
         fatalError("Not implemented")
     }
     
-    private let label = UILabel(font: .preferredFont(forTextStyle: .title3).bold())
+    private let label = UILabel()
     private let button = UIButton()
     
     private lazy var stackView = UIStackView(arrangedSubviews: [label, UIView(), button])
@@ -21,14 +21,18 @@ final class RecentlyViewedHeader: UICollectionReusableView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        setupSubviews()
+        setupSubviewProperties()
+        addSubviews()
     }
     
-    private func setupSubviews() {
+    private func setupSubviewProperties() {
+        setupLabel(font: .preferredFont(forTextStyle: .title3).bold())
+        
         label.text = "Recently viewed"
         button.setTitle("CLEAR", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.titleLabel?.font = .preferredFont(forTextStyle: .callout).bold()
+        button.titleLabel?.adjustsFontForContentSizeCategory = true
         button.backgroundColor = UIColor(named: "clear-button-gray")
         button.contentEdgeInsets = .init(top: 5, left: 10, bottom: 5, right: 10)
         
@@ -38,7 +42,18 @@ final class RecentlyViewedHeader: UICollectionReusableView {
         
         stackView.layoutMargins = .init(top: 10, left: 0, bottom: 0, right: 0)
         stackView.isLayoutMarginsRelativeArrangement = true
-        
+    }
+    
+    private func setupLabel(font: UIFont, textColor: UIColor = .label, textAlignment: NSTextAlignment = .center) {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = font
+        label.textColor = textColor
+        label.textAlignment = textAlignment
+        label.adjustsFontForContentSizeCategory = true
+        label.numberOfLines = 0
+    }
+    
+    private func addSubviews() {
         addSubview(stackView)
         
         NSLayoutConstraint.activate([
@@ -47,6 +62,24 @@ final class RecentlyViewedHeader: UICollectionReusableView {
             stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
             stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        let isAccessibilityCategory = traitCollection.preferredContentSizeCategory.isAccessibilityCategory
+        if isAccessibilityCategory != previousTraitCollection?.preferredContentSizeCategory.isAccessibilityCategory {
+            updateStackViewAxis()
+        }
+    }
+    
+    private func updateStackViewAxis() {
+        let isAccessibilityCategory = traitCollection.preferredContentSizeCategory.isAccessibilityCategory
+        if isAccessibilityCategory {
+            stackView.axis = .vertical
+        } else {
+            stackView.axis = .horizontal
+        }
     }
     
 }
